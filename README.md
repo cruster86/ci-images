210573b409357374f2cf8db4c4bc671fdc451815
Vladislav Lesnik <zerodistance@sirius.online>
2023-12-29 08:58:08 +0000

    update ansible ci image for jobs

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
   Релизы:
   - v7.0.0 - базовый образ с ansible и python
   - v7.0.1 - добавлен модуль amazon.aws, преднастроен ansible-vault и ключ шифрования /ansible/vaultpass
-  - v7.1.0 - обновлены: alpine3.19, ansible 7.0.0 (ansible-core 2.14), ansible-lint 6.22.1
+  - v7.1.0 - обновлены: alpine3.19, ansible 7.1.0 (ansible-core 2.14.13), ansible-lint 6.22.1
   ```
   ```
   Для проектов:
--- ansible/dockerbuild.sh
+++ ansible/dockerbuild.sh
@@ -2,6 +2,6 @@
 # $IMAGE_NAME var is injected into the build so the tag is correct.
 
 docker build --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
-             --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION:=7.0.0} \
+             --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION:=7.1.0} \
              --build-arg ANSIBLE_LINT_VERSION=${ANSIBLE_LINT_VERSION:=6.22.1} \
-             -t ${IMAGE_NAME:=registry.sirius.online/infra/deploy/docker-ansible:v7.1.0} .
+             -t ${IMAGE_NAME:=registry.sirius.online/infra/images/docker-ansible:v7.1.0} .
