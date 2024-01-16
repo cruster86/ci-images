54750284eaab2c75cd776671b7feec323d3f9952
Vladislav Lesnik <zerodistance@sirius.online>
2024-01-16 08:28:37 +0000

    update dind-kubectl dockerfile

--- dind-kubectl/dockerbuild.sh
+++ dind-kubectl/dockerbuild.sh
@@ -2,4 +2,4 @@
 # $IMAGE_NAME var is injected into the build so the tag is correct.
 
 docker build --build-arg KUBECTL_VERSION=${KUBECTL_VERSION:=v1.21.2} \
-             -t ${IMAGE_NAME:=registry.sirius.online/infra/images/ansible/dind-kubectl:v2.0.0} .
+             -t ${IMAGE_NAME:=registry.sirius.online/infra/images/dind-kubectl:v2.0.0} .
